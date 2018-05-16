530d1fdba0c38b3dc2dfc6bcedc8b6dc0339a616
Marcio Alexandre Macedo Rodrigues <marcioamr@MacBook-Pro-de-Marcio.local>
2018-05-15 22:51:05 -0300

    Fix

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
 	var registry = app.controllers.registryController;
 
-	app.get('getToken', registry.getToken);
-	app.post('addDocument', registry.addDocument);
-	app.post('searchDocument' , registry.searchDocument);
+	app.get('/getToken', registry.getToken);
+	app.post('/addDocument', registry.addDocument);
+	app.post('/searchDocument' , registry.searchDocument);
 	
 };
