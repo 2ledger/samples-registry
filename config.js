fbdba5ed7ec76b96ee1d3cd7baaf92207be6e028
Marcio Alexandre Macedo Rodrigues <marcioamr@MacBook-Pro-de-Marcio.local>
2018-05-18 13:36:08 -0300

    Adjust on API url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var development = {
 	env : global.process.env.NODE_ENV || 'development',
 	port : global.process.env.PORT || 8080,
 	host : global.process.env.HOST || 'localhost',
-	API_2LEDGER : global.process.env.API_2LEDGER || '2ledger-api-dev.azurewebsites.net',
+	API_2LEDGER : global.process.env.API_2LEDGER || 'sandbox.2ledger.com',
 	API_2LEDGER_TOKEN : global.process.env.API_2LEDGER_TOKEN || '.',
 	API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID : global.process.env.API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID || '5ae26d0edb26860036e5bda2'
 };
